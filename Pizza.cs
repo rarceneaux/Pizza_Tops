9dd8d0e7830683975a46cda582c84a391bf8bcff
Raymond Arceneaux <raymond.techcoder@gmail.com>
2020-03-23 20:41:19 -0500

    Add project files.

---
+++ Pizza.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizza_Tops
+{
+    class Pizza
+    {
+        public List<string> Toppings { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+namespace Pizza_Tops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JsonConvert.DeserializeObject<>(File.ReadAllText(@"c:\movie.json"));
+
+        }
+    }
+}
